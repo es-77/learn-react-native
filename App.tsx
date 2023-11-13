ae69dddbd6b78bd64ad4aa09ce44b7957fd95ff4
Emmanuel <emmanuelazlaan@gmail.com>
2023-11-13 09:40:57 -0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize:50  }}>Emmanuel saleem</Text>
+      <Text style={{ fontSize:50  }}>second text</Text>
+    </View>
+  );
+}
+
+export default App;
