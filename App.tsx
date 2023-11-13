96da35ca543555689c4148e015a3e4bca58e3ea9
Emmanuel <emmanuelazlaan@gmail.com>
2023-11-13 10:03:20 -0700

    button view text

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import {View,Text} from 'react-native';
-
+import {View, Text} from 'react-native';
+import PageViewTextButton from './components/PageViewTextButton';
 
 const App = () => {
   return (
     <View>
-      <Text style={{ fontSize:50  }}>Emmanuel saleem</Text>
-      <Text style={{ fontSize:50  }}>second text</Text>
+      <Text style={{fontSize: 50}}>Emmanuel saleem</Text>
+      <Text style={{fontSize: 50}}>second text</Text>
+      <PageViewTextButton/>
     </View>
   );
-}
+};
 
 export default App;
---
+++ components/PageViewTextButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View, Text, Button} from 'react-native'
+const PageViewTextButton = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 50}}>other components</Text>
+      <Button title="click me" ></Button>
+      <Text style={{fontSize: 30}}>other components another button</Text>
+      <Button title="another button" ></Button>
+    </View>
+  );
+};
+
+export default PageViewTextButton;
